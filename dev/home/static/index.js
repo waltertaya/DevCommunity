d12f55e0ae94566a01e37bcf315ba740e01bed86
waltertaya <waltermittybecker5@gmail.com>
2023-11-14 22:39:26 +0300

    updated

--- dev/home/static/index.js
+++ dev/home/static/index.js
@@ -2,5 +2,5 @@ let bg_color = document.querySelector('label');
 let search_input = document.getElementById('search');
 let search_btn = document.getElementById('btn3')
 function mean() {
-    search_btn.style.display = 'none';
+    search_btn.style.border = 'blue';
 }
