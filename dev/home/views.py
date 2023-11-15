fe5cd292f7dc517fae22f231e14d5aa2487b325e
waltertaya <waltermittybecker5@gmail.com>
2023-11-15 22:22:43 +0300

    started backend

--- dev/home/views.py
+++ dev/home/views.py
@@ -1,4 +1,31 @@
 from django.shortcuts import render
+from .models import Post
+
+posts = [
+    {'author': '',
+     'title': '',
+     'content': '',
+     'date_posted': ''
+     },
+    {'author': '',
+     'title': '',
+     'content': '',
+     'date_posted': ''
+     },
+    {'author': '',
+     'title': '',
+     'content': '',
+     'date_posted': ''
+     },
+    {'author': '',
+     'title': '',
+     'content': '',
+     'date_posted': ''
+     }
+]
 
 def home_page(request):
-    return render(request, 'home.html')
+    context = {
+        'posts' : Post.objects.all()
+    }
+    return render(request, 'home.html', context)
