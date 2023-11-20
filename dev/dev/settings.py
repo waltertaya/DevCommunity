9d050aa54987351e86bf24d32477e5a5b7ef5c73
waltertaya <waltermittybecker5@gmail.com>
2023-11-21 00:10:42 +0300

    updated

--- dev/dev/settings.py
+++ dev/dev/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'login',
         'USERNAME' : 'postgres',
-        'PASSWORD' : 'yourpassword',
+        'PASSWORD' : '',
         'HOST' : 'localhost',
     }
 }
