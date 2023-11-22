fc8a9ff09bd428b988b2ca2dae26c5828ecd3ba8
waltertaya <waltermittybecker5@gmail.com>
2023-11-22 18:07:35 +0300

    updated

--- dev/logins/views.py
+++ dev/logins/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 
-def def register(request):
+def register(request):
     form  = UserCreationForm()
-    return render(request, 'register.html', {'form': form})
+    return render(request, 'login/register.html', {'form': form})
     
+
+def login(request):
+    return render(request, 'login/login.html')
